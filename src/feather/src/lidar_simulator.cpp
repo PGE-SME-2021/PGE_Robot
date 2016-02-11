36a099bb76994a83d780c6940452921280c53fa7
camara-cheikh <cheikhtidianecamara1@gmail.com>
2016-02-11 20:21:15 +0000

    Saving Changes

--- src/feather/src/lidar_simulator.cpp
+++ src/feather/src/lidar_simulator.cpp
@@ -2,49 +2,63 @@
 #include "std_msgs/String.h"
 #include "feather/LidarData.h"
 
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/PoseStamped.h>
+#include "nav_msgs/Path.h"
+
 #include <sstream>
+//float traject[80]={};
+float* coordonnees;
+void trajectorycallback (const nav_msgs::Path traject)
+{
+	ROS_INFO("It is good %d", traject);
+	// coordonnees= traject;
+
+}
 
 int main(int argc, char **argv)
 {
-float x=0, y=0;
-int i, count;
 
 ros::init(argc, argv, "lidar_sim_cpp");
+ros::init(argc, argv, "sub_cartesian");
 
 ros::NodeHandle n;
-
+ros:: Subscriber subsc=n.subscribe("trajectory",10,trajectorycallback);
 ros::Publisher chatter_pub = n.advertise<feather::LidarData>("lidar_points", 1000);
-
-ros::Rate loop_rate(10);
-count = 0;
+float x=0, y=0;
+int i, count;
+//float *coor;
+//float** traject;
+//std::vector<geometry_msgs::PoseStamped> const & poses = &traject;
+//ros::Rate loop_rate(10);
 while (ros::ok()){
-	x = count; y = count - 30;
 	feather::LidarData msg;
-
-	/*
-	std::stringstream ss;
-	ss << "hello world " << count;
-	msg.data = ss.str();
-	*/
+	nav_msgs::Path coordonnees;
+	//coor= &trajectory_.trajectory.poses;
 	
-	for(i = 0; i < 80; i++){
-		x = x + 1;
-		y = y + 1;
-		msg.points[i].x = x;
-		msg.points[i].y = y;
+	//std::stringstream coor;
+	//std:: << "yes" << coor;
+	//msg.data = coor;
+
+
+	
+
+	ROS_INFO("%f", coordonnees);
+
+	for(i = 0; i < 1000; i++){
+		msg.points[i].x = 5;
+		msg.points[i].y = 5;
 		msg.points[i].z = 0;
-		}	
+		}
 
-	ROS_INFO("%f %f", msg.points[0].x, msg.points[0].y);
+	//ROS_INFO("%f %f", msg.points[0].x, msg.points[0].y);
 
-	chatter_pub.publish(msg);
+	chatter_pub.publish(coordonnees);
 
 	ros::spinOnce();
 
-	loop_rate.sleep();
-	count++;
-	if(count > 200) count = 0;
-	}
+	//loop_rate.sleep();
+}	
 
 return 0;
 

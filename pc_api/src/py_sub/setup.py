72c1b674d17e34c74058d1415aeee9aeb30734d6
Melet <melet.chirino@gmail.com>
2022-01-06 15:30:01 +0100

    [ADD] Nodes that send data in list #1

---
+++ pc_api/src/py_sub/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'py_sub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='meletc',
+    maintainer_email='melet.chirino@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = py_sub.publisher_member_function:main',
+            'subscriber = py_sub.subscriber_member_function:main',
+        ],
+    },
+)

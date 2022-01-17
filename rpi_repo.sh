fe48cd2854405754bbe8af086c415abdc3387caf
Melet David <melet.chirino@gmail.com>
2022-01-17 12:22:30 +0100

    [FIX] git commands in repo installer #22

--- rpi_repo.sh
+++ rpi_repo.sh
@@ -2,6 +2,6 @@ git config --global user.name "FeatherBot"
 git config --global user.email "feather.youpmail.com"
 cd ~/catkin_ws/
 git init
-git add remote origin https://github.com/PGE-SME-2021/GUI_Robot.git
+git remote add origin https://github.com/PGE-SME-2021/GUI_Robot.git
 git pull origin main --allow-unrelated-histories
 

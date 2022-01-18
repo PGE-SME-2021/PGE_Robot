487478e739770153a185eb4071c1fa0f1e7d2761
Melet <melet.chirino@gmail.com>
2022-01-18 23:10:28 +0100

    [ADD] points msg, dependencies and receiver node

---
+++ src/feather/src/lidar_sim_recv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rospy
+#from std_msgs.msg import Int32MultiArray
+from feather.msg import LidarData
+
+class NodeSub():
+    def __init__(self, topic_name, message_type, node_name):
+        self.topic_name = topic_name
+        self. message_type = message_type
+        self.node_name = node_name
+        self.data = LidarData()
+
+    def start_node(self):
+        rospy.init_node(self.node_name)
+
+    def subscriber(self):
+        print("Creating sub node")
+        sub = rospy.Subscriber(
+                self.topic_name,
+                self.message_type,
+                self.callback_function
+                )
+        print("spining sub node")
+        rospy.spin()
+
+    def callback_function(self, message):
+        self.data = message
+        print("result:")
+        rospy.loginfo(F"GOT {self.data.points[0]}")
+
+
+if __name__ == "__main__":
+    node_sub = NodeSub('lidar_points', LidarData, 'lidar_recv')
+    node_sub.start_node()
+    node_sub.subscriber()
+
--- src/feather/src/lidar_simulator.py
+++ src/feather/src/lidar_simulator.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 import rospy
 import time
-from std_msgs.msg import Int32MultiArray
+#from std_msgs.msg import Int32MultiArray
+from feather.msg import LidarData
 
 from random import randrange
 
 def publisher():
-    pub = rospy.Publisher("lidar_points", Int32MultiArray, queue_size = 10)
+    pub = rospy.Publisher("lidar_points", LidarData, queue_size = 10)
     rate = rospy.Rate(3)#1Hz
-    counter = 0
-    battery_level = 50
-    msg_to_publish = Int32MultiArray()
-    points_number = 70
+    msg_to_publish = LidarData()
+    points_number = 80
     while not rospy.is_shutdown():
-        points = []
-        p = [4, 4]
-        points.append(p)
+        x = 0
+        y = 0
         for i in range(points_number):
             delta_x = randrange(-5, 5)
             delta_y = randrange(-5, 5)
-            p = [p[0] + delta_x, p[1] + delta_y]
-            points.append(p)
+            x += delta_x
+            y += delta_y
+            msg_to_publish.points[i].x = x
+            msg_to_publish.points[i].y = y
+            msg_to_publish.points[i].z = 0
 
-        msg_to_publish.data = points
         pub.publish(msg_to_publish)
-        rospy.loginfo(points)
+        rospy.loginfo(msg_to_publish)
         time.sleep(3)
 
 if __name__ == "__main__":

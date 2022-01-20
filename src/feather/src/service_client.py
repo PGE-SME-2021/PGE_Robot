2dc74da45359d2441321698b15978c176c562f2d
Melet <melet.chirino@gmail.com>
2022-01-20 12:27:04 +0100

    [ADD] Sum 3 ints service

--- src/feather/src/service_client.py
+++ src/feather/src/service_client.py
@@ -5,11 +5,11 @@ import sys
 import rospy
 from feather.srv import *
 
-def add_two_ints_client(x, y):
-    rospy.wait_for_service('add_two_ints')
+def add_two_ints_client(x, y, z):
+    rospy.wait_for_service('add_3')
     try:
-        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
-        resp1 = add_two_ints(x, y)
+        add_two_ints = rospy.ServiceProxy('add_3', AddThreeInts)
+        resp1 = add_two_ints(x, y, z)
         return resp1.sum
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
@@ -18,11 +18,8 @@ def usage():
     return "%s [x y]"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-    else:
-        print(usage())
-        sys.exit(1)
+    x = 1
+    y = 34
+    z = 3
     print("Requesting %s+%s"%(x, y))
-    print("%s + %s = %s"%(x, y, add_two_ints_client(x, y)))
+    print("%s + %s = %s"%(x, y, add_two_ints_client(x, y, z)))

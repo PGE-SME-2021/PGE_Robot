2a08a62c73818853001dc185783c9d3a67ae7806
Juan Beroy <juan.beroy1@gmail.com>
2022-01-20 15:07:26 +0100

    Updated mainwindow_gui to test service with button

--- src/feather/src/service_client.py
+++ src/feather/src/service_client.py
@@ -18,11 +18,7 @@ def usage():
     return "%s [x y]"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-    else:
-        print(usage())
-        sys.exit(1)
+    x = 1
+    y = 2
     print("Requesting %s+%s"%(x, y))
     print("%s + %s = %s"%(x, y, add_two_ints_client(x, y)))

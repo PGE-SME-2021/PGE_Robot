194be300f245251adbf67a97c750f5ea2d19fcd4
Melet <melet.chirino@gmail.com>
2022-01-24 18:31:18 +0100

    [RENAME] functions #34

--- src/feather/src/communication.cpp
+++ src/feather/src/communication.cpp
@@ -2,41 +2,35 @@
 // library
 #include "ros/ros.h"
 
-#include "serial/serial.h"
+//#include "serial/serial.h"
 #include "stdio.h"
 #include "feather/SendCommand.h"
 #include "serial_cmd.cpp"
 
 
 
-bool add(feather::SendCommand::Request  &req,
+bool raspi_to_esp(feather::SendCommand::Request  &req,
          feather::SendCommand::Response &res)
-{
+{ 
 
 	if ((0 <= req.com) && (req.com <= 7)) {
-			
 
-	send_serial(req.com);
+		send_serial(req.com);
       	res.check = 1;
     } else  {
         res.check = 0;
     }
     
-    //ROS_INFO("request: com=%ld", (long int)req.com);
-    //ROS_INFO("sending back response: [%ld]", (long  int)res.check);
+    ROS_INFO("request: com=%ld", (long int)req.com);
+    ROS_INFO("sending back response: [%ld]", (long  int)res.check);
   return true;
 }
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "communication");
   ros::NodeHandle n;
-  
-  
- 
- 
- 
-  ros::ServiceServer service = n.advertiseService("send_command", add);
-  ROS_INFO("Ready to send commands."); // = printf 
+  ros::ServiceServer service = n.advertiseService("send_command", raspi_to_esp);
+  ROS_INFO("Ready to send commands to esp."); // = printf 
   ros::spin();
 
   return 0;

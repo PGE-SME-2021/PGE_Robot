55f4888011badb79f52b397ee89997c634ffcf89
Melet <melet.chirino@gmail.com>
2022-01-28 14:48:07 +0100

    [UPDATE] lidar sim receiver

--- src/feather/src/lidar_sim_recv.py
+++ src/feather/src/lidar_sim_recv.py
@@ -2,34 +2,27 @@
 import rospy
 #from std_msgs.msg import Int32MultiArray
 from feather.msg import LidarData
-
-class NodeSub():
-    def __init__(self, topic_name, message_type, node_name):
-        self.topic_name = topic_name
-        self. message_type = message_type
-        self.node_name = node_name
-        self.data = LidarData()
-
-    def start_node(self):
-        rospy.init_node(self.node_name)
-
-    def subscriber(self):
-        print("Creating sub node")
-        sub = rospy.Subscriber(
-                self.topic_name,
-                self.message_type,
-                self.callback_function
-                )
-        print("spining sub node")
-        rospy.spin()
-
-    def callback_function(self, message):
-        self.data = message
-        print("result:")
-        rospy.loginfo("GOT %d"%self.data.points[0])
+from std_msgs.msg import String
+from nav_msgs import Path
 
 
-if __name__ == "__main__":
-    node_sub = NodeSub('lidar_points', LidarData, 'lidar_recv')
-    node_sub.start_node()
-    node_sub.subscriber()
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+    import pdb; pdb.set_trace()
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener')
+
+    rospy.Subscriber("trajectory", Path, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()

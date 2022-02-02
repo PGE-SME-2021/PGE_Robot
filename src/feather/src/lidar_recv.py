d586cfa87f98a0d4f51921964d88177a5bf6ed05
Melet <melet.chirino@gmail.com>
2022-02-02 12:25:06 +0100

    [UPDATE] Lidar receiver can save data in csv files

--- src/feather/src/lidar_recv.py
+++ src/feather/src/lidar_recv.py
@@ -9,19 +9,18 @@ import inspect
 
 from csv_functions import *
 
-POINTS = LidarData()
+#POINTS = LidarData()
 def callback(data):
     new_points = data.points
     print("got %d points" % len(new_points))
     print("new points = %d , %d" % (new_points[80].x, new_points[83].y))
     csv_name = generate_file_name()
-    #import pdb; pdb.set_trace()
-    save_csv_data(csv_name, data.points)
+    save_csv_data(csv_name, data)
 
 """
 ['__class__', '__delattr__', '__doc__', '__eq__', '__format__', '__getattribute__', '__getstate__', '__hash__', '__init__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', '_check_types', '_connection_header', '_full_text', '_get_types', '_has_header', '_md5sum', '_slot_types', '_type', 'channels', 'deserialize', 'deserialize_numpy', 'header', 'points', 'serialize', 'serialize_numpy']
 """
- 
+
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same

83be16b101485c724f853312e431230786852cfd
Melet <melet.chirino@gmail.com>
2022-02-02 12:27:01 +0100

    [UPDATE] publisher and subscriber in cpp

--- src/feather/src/publisher_cpp.cpp
+++ src/feather/src/publisher_cpp.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
 
 #include <sstream>
 
@@ -9,19 +10,19 @@ ros::init(argc, argv, "talker");
 
 ros::NodeHandle n;
 
-ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+ros::Publisher chatter_pub = n.advertise<std_msgs::Int32>("chatter", 1000);
 
-ros::Rate loop_rate(10);
+ros::Rate loop_rate(0.5);
 
 int count = 0;
 while (ros::ok()){
-	std_msgs::String msg;
+	std_msgs::Int32 msg;
 
 	std::stringstream ss;
 	ss << "hello world " << count;
-	msg.data = ss.str();
+	msg.data = count;
 
-	ROS_INFO("%s", msg.data.c_str());
+	ROS_INFO("%d", msg.data);
 
 	chatter_pub.publish(msg);
 

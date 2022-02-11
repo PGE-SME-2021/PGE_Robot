739b3ecce6e332393d8a9619660dbc586eee849d
aimen-akrouf <aymen.akrouf@gmail.com>
2022-02-11 12:06:03 +0100

    This code allows testing the control of one Motor via L298N

--- esp-files/ledc/main/ledc_example_main.c
+++ esp-files/ledc/main/ledc_example_main.c
@@ -7,14 +7,24 @@
 #include <stdio.h>
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+
+
+#define GPIO_1 15
+#define GPIO_2 16
+
 
 #define LEDC_TIMER              LEDC_TIMER_0
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO          (20) // Define the output GPIO
+#define LEDC_OUTPUT_IO          (14) // Define the output GPIO
 #define LEDC_CHANNEL            LEDC_CHANNEL_0
-#define LEDC_DUTY_RES           LEDC_TIMER_9_BIT // Set duty resolution to 9 bits
-#define LEDC_DUTY               (440) // Set duty to 80%. ((2 ** 9) - 1) * XX% =~440 
-#define LEDC_FREQUENCY          (20300) // Frequency in Hertz. Set frequency at 20.3 kHz 
+#define LEDC_DUTY_RES           LEDC_TIMER_8_BIT // Set duty resolution to 9 bits
+#define LEDC_DUTY               (255) // Set duty to 80%. ((2 ** 9) - 1) * XX% =~440 
+#define LEDC_FREQUENCY          (30000) // Frequency in Hertz. Set frequency at 20.3 kHz 
                                         // needs to be coherent with the resolution
 
 static void example_ledc_init(void)
@@ -44,10 +54,32 @@ static void example_ledc_init(void)
 
 void app_main(void)
 {
+gpio_reset_pin(GPIO_1);
+gpio_reset_pin(GPIO_2);
+
+gpio_set_direction(GPIO_1, GPIO_MODE_OUTPUT);
+gpio_set_direction(GPIO_2, GPIO_MODE_OUTPUT);
+
     // Set the LEDC peripheral configuration
     example_ledc_init();
     // Set duty to 50%
     ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
     // Update duty to apply the new value
     ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+    while(1){
+        printf("Motor go forward\n");
+        gpio_set_level(GPIO_1, 0);
+        gpio_set_level(GPIO_2, 1);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        printf("Motor stops \n");
+        gpio_set_level(GPIO_1, 0);
+        gpio_set_level(GPIO_2, 0);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        printf("Motor go backward\n");
+        gpio_set_level(GPIO_1, 1);
+        gpio_set_level(GPIO_2, 0);
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
 }

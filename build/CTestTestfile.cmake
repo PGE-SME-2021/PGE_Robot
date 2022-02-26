d63a8293dfb20210c4ed0ae68fbe5dc9149b7425
Mcuillerat <78605052+Mcuillerat@users.noreply.github.com>
2022-02-26 12:44:05 +0100

    Update .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/test05fev/src
-# Build directory: /home/ubuntu/test05fev/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(cam)
--- build/cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/test05fev/src/cam
-# Build directory: /home/ubuntu/test05fev/build/cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
